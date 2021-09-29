caaccfe99bd9ff0914ce74666fe37b55f9e63ca8
Taif Ahmad <a7x.taif@gmail.com>
2021-09-29 09:51:31 +0530

    made some improvements

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
+let grid = document.querySelector('#grid')
+let erase = document.querySelector('#erase')
 
-let parentDiv = document.querySelector('#parentDiv')
-
-function createGrid(parent) {
-    for (let i = 0; i < 256; i++) {
-        let div = document.createElement('div');
+function createGrid(size) {
+    for (let i = 0; i < (size *size); i++) {
+        let div = document.createElement('div')
+        div.style.border = '1px solid grey';
         div.addEventListener('mouseenter', () => {
-            div.style.backgroundColor = "white"
+            div.style.backgroundColor = 'red'
         })
-        parent.appendChild(div)
+        erase.addEventListener('click', () => {
+            div.style.backgroundColor = 'white'
+        })
+        grid.appendChild(div)
     }
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
 }
-createGrid(parentDiv)
+
+createGrid(100)
+
+
+
 
 
 
